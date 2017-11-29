5be0eff3099a1ff4a9fc93562a7435cedc9fb75f
VidgarVii <vidasp@mail.ru>
2017-11-29 19:56:10 +0300

    asfd

--- index.php
+++ index.php
@@ -3,31 +3,33 @@
 include 'head.html'; 
 include 'nav.php'; 
 
-switch ($_GET['page']){
-    case 'index':
-       include 'body.html';
-        break;
-    case 'crown':
-       include 'crown.html';
-        break;
-    case 'earrings':
-       include 'earrings.html';
-        break;
-    case 'pendant':
-       include 'pendant.html';
-        break;
-    case 'photogallery':
-       include 'photogallery.html';
-        break;
-    case 'kontaсt':
-       include 'kontaсt.html';
-        break;
-    default:
-        include 'body.html';
+if(isset($_GET['page'])){
+    switch ($_GET['page']){
+        case 'index':
+           include 'page/body.html';
+            break;
+        case 'crown':
+           include 'page/crown.html';
+            break;
+        case 'earrings':
+           include 'page/earrings.html';
+            break;
+        case 'pendant':
+           include 'page/pendant.html';
+            break;
+        case 'photogallery':
+           include 'page/photogallery.html';
+            break;
+        case 'kontaсt':
+           include 'page/kontact.html';
+            break;
+        default: case 'index':
+           include 'page/body.html';
+    }
+}
+else {
+    include 'body.html';
 }
 
 
 include 'footer.html';
- 
-
-   
--- nav.php
+++ nav.php
@@ -1,4 +1,4 @@
- <nav class="nav justify-content-center">
+ <nav class="nav text-center justify-content-center">
             <a class="nav-link" href="?page=index">Главная</a>
             <a class="nav-link" href="?page=crown">Короны</a>
             <a class="nav-link" href="?page=earrings">Серьги</a>
@@ -6,3 +6,4 @@
             <a class="nav-link" href="?page=photogallery">Фотогаллерея</a>
             <a class="nav-link" href="?page=kontaсt">Контакты</a>
         </nav>
+        <section>
