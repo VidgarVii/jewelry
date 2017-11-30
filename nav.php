942c6b45d4f0fde9a31391deee690c922f23c29f
VidgarVii <vidasp@mail.ru>
2017-11-30 22:14:11 +0300

    asd

--- nav.php
+++ nav.php
@@ -1,11 +1,38 @@
- <nav class="nav text-center justify-content-center">
+<nav class= " p-0 text-center justify-content-center navbar navbar-expand-md">
          
-            <a class="nav-link" href="?page=index">Главная</a>
-            <a class="nav-link" href="?page=crown">Короны</a>
-            <a class="nav-link" href="?page=earrings">Серьги</a>
-            <a class="nav-link" href="?page=pendant">Подвески</a>
-            <a class="nav-link" href="?page=photogallery">Фотогаллерея</a>
-            <a class="nav-link" href="?page=kontaсt">Контакты</a>
-     
-        </nav>
-        <section>
+     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+     <svg viewBox="0 0 800 600" class="">
+  <path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" class="top_bar" />
+  <path d="M300,320 L540,320" class="middle_bar" />
+  <path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" class="bottom_bar" transform="translate(480, 320) scale(1, -1) translate(-480, -318)" />
+</svg>
+        
+      </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            
+              <ul class="navbar-nav mr-auto">
+               
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=index">Главная  <span class="sr-only">(current)</span></a>
+                </li>
+                
+               <li class="nav-item">
+                    <a class="nav-link" href="?page=crown">Короны</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=earrings">Серьги</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=pendant">Подвески</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=photogallery">Фотогаллерея</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?page=kontaсt">Контакты</a>
+                </li>
+              </ul>
+            </div>
+</nav>
+<section>
